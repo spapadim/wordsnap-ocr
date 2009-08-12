43a933c08a9b8bd66de5a43ce5b43feef1f1ddb2
spapadim <spapadim@b9292112-8e80-11de-9a8c-6d5b7d1e4d13>
2009-08-12 02:00:42 +0000

    First complete implementation of PreviewImage and associated native methods; removed old benchmarking code.

---
+++ jni/net_bitquill_ocr_PreviewImage.cpp
@@ -0,0 +1,248 @@
+#include <jni.h>
+#include "net_bitquill_ocr_PreviewImage.h"
+
+static void throwException (JNIEnv *env, const char* ex, const char* msg)
+{
+    if (jclass cls = env->FindClass(ex)) {
+        env->ThrowNew(cls, msg);
+        env->DeleteLocalRef(cls);
+    }
+}
+
+template <class T>
+inline static T getPixel (const T data[], int width, int i, int j)
+{
+    return data[i*width + j];
+}
+
+template <class T>
+inline static void setPixel (T data[], int width, int i, int j, T value)
+{
+    data[i*width + j] = value;
+}
+
+template <class T>
+inline static T min (T a, T b)
+{
+    return (a < b) ? a : b;
+}
+
+template <class T>
+inline static T max (T a, T b)
+{
+    return (a > b) ? a : b;
+}
+
+static void rowSumIncDec (int* sum,
+        int rowInc, int rowDec,
+        const unsigned char* data,
+        int width, int height, int radius)
+{
+    int deltaInc = 0, deltaDec = 0;
+
+    // Handle first element
+    for (int sj = 0;  sj <= radius;  sj++) {
+        if (rowInc < height) {
+            deltaInc += getPixel(data, width, rowInc, sj);
+        }
+        if (rowDec >= 0) {
+            deltaDec += getPixel(data, width, rowDec, sj);
+        }
+    }
+    sum[0] += deltaInc - deltaDec;
+
+    // Incrementally deal with remaining elements
+    // Broken into three for loops to avoid if statements inside loop
+    for (int j = 1;  j < radius + 1;  j++) {
+        // Left edge
+        if (rowInc < height) {
+            deltaInc += getPixel(data, width, rowInc, j+radius);
+        }
+        if (rowDec >= 0) {
+            deltaDec += getPixel(data, width, rowDec, j+radius);
+        }
+        sum[j] += deltaInc - deltaDec;
+    }
+    for (int j = radius + 1;  j < width - radius;  j++) {
+        // Internal
+        if (rowInc < height) {
+            deltaInc -= getPixel(data, width, rowInc, j-radius-1);
+            deltaInc += getPixel(data, width, rowInc, j+radius);
+        }
+        if (rowDec >= 0) {
+            deltaDec -= getPixel(data, width, rowDec, j-radius-1);
+            deltaDec += getPixel(data, width, rowDec, j+radius);
+        }
+        sum[j] += deltaInc - deltaDec;
+    }
+    for (int j = width - radius;  j < width;  j++) {
+        // Right edge
+        if (rowInc < height) {
+            deltaInc -= getPixel(data, width, rowInc, j-radius-1);
+        }
+        if (rowDec >= 0) {
+            deltaDec -= getPixel(data, width, rowDec, j-radius-1);
+        }
+        sum[j] += deltaInc - deltaDec;
+    }
+}
+
+static void avgRow (const int* sum, int row,
+        unsigned char* data, int width, int height, int radius)
+{
+    // Compute clipped height
+    int h = min(height-1, row+radius) - max(0, row-radius) + 1;
+    for (int j = 0;  j < width;  j++) {
+        // Compute clipped width
+        int w = min(width-1, j+radius) - max(0, j-radius) + 1;
+        setPixel(data, width, row, j, (unsigned char)(sum[j]/(w*h)));
+    }
+}
+
+JNIEXPORT void JNICALL Java_net_bitquill_ocr_PreviewImage_nativeMeanFilter
+  (JNIEnv *env, jclass cls, jbyteArray jin, jbyteArray jout,
+          jint width, jint height, jint radius)
+{
+    // Check parameters
+    if (width < 0 || height < 0) {
+        throwException(env, "java/lang/IllegalArgumentException", "Width and height must be non-negative");
+        return;
+    }
+    if (env->GetArrayLength(jin) < width*height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Input array too short");
+        return;
+    }
+    if (env->GetArrayLength(jout) < width*height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Output array too short");
+        return;
+    }
+    int* sum = new int[width];
+    if (sum == 0) {
+        throwException(env, "java/lang/OutOfMemoryError", "Failed to allocate sums buffer");
+        return;
+    }
+
+    unsigned char *in = (unsigned char *) env->GetByteArrayElements(jin, 0);
+    unsigned char *out = (unsigned char *) env->GetByteArrayElements(jout, 0);
+
+    for (int si = 0;  si <= radius;  si++) {
+        rowSumIncDec(sum, si, -1, in, width, height, radius);
+    }
+    avgRow(sum, 0, out, width, height, radius);
+
+    for (int i = 1;  i < height; i++) {
+        rowSumIncDec(sum, i+radius, i-radius-1, in, width, height, radius);
+        avgRow(sum, i, out, width, height, radius);
+    }
+
+    delete sum;
+    env->ReleaseByteArrayElements(jin, (jbyte *)in, 0);
+    env->ReleaseByteArrayElements(jout, (jbyte *)out, 0);
+}
+
+JNIEXPORT jint JNICALL Java_net_bitquill_ocr_PreviewImage_nativeMean
+  (JNIEnv *env, jclass cls, jbyteArray jin, jint width, jint height)
+{
+    // Check parameters
+    if (width < 0 || height < 0) {
+        throwException(env, "java/lang/IllegalArgumentException", "Width and height must be non-negative");
+        return -1;
+    }
+    if (env->GetArrayLength(jin) < width*height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Input array too short");
+        return -1;
+    }
+
+    unsigned char *in = (unsigned char *) env->GetByteArrayElements(jin, 0);
+
+    int total = 0;
+    for (int i = 0;  i < height;  i++) {
+        for (int j = 0;  j < width;  j++) {
+            total += getPixel(in, width, i, j);
+        }
+    }
+
+    env->ReleaseByteArrayElements(jin, (jbyte *)in, 0);
+
+    return total / (width * height);
+}
+
+JNIEXPORT void JNICALL Java_net_bitquill_ocr_PreviewImage_nativeGrayToARGB
+  (JNIEnv *env, jclass cls,
+          jbyteArray jin, jint imgWidth, jint imgHeight,
+          jintArray jout, jint left, jint top, jint width, jint height)
+{
+    // Check parameters
+    if (width < 0 || height < 0 || imgWidth < 0 || imgHeight < 0) {
+        throwException(env, "java/lang/IllegalArgumentException", "Width and height must be non-negative");
+        return;
+    }
+    if (env->GetArrayLength(jout) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Output array too short");
+        return;
+    }
+    if (env->GetArrayLength(jin) < imgWidth * imgHeight) {
+        throwException(env, "java/lang/IllegalArgumentException", "Input array too short");
+        return;
+    }
+    if (left < 0 || left + width >= imgWidth || top < 0 || top + height >= imgHeight) {
+        throwException(env, "java/lang/IllegalArgumentException", "ROI exceeds image");
+        return;
+    }
+
+    unsigned char *in = (unsigned char *) env->GetByteArrayElements(jin, 0);
+    unsigned int *out = (unsigned int *) env->GetIntArrayElements(jout, 0);
+
+    for (int i = 0;  i < height;  i++) {
+        for (int j = 0;  j < width;  j++) {
+            unsigned char gray = getPixel(in, imgWidth, top + i, left + j);
+            unsigned int argb = (0xFF << 24) | (gray << 16) | (gray < 8) | gray;
+            setPixel(out, width, i, j, argb);
+        }
+    }
+
+    env->ReleaseByteArrayElements(jin, (jbyte *)in, 0);
+    env->ReleaseIntArrayElements(jout, (jint *)out, 0);
+}
+
+JNIEXPORT void JNICALL Java_net_bitquill_ocr_PreviewImage_nativeAdaptiveThreshold
+  (JNIEnv *env, jclass cls,
+    jbyteArray jin, jbyteArray jthresh, jbyteArray jout,
+    jint width, jint height, jbyte hi, jbyte lo, jint offset)
+{
+    // Check parameters
+    if (env->GetArrayLength(jin) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Input array too short");
+        return;
+    }
+    if (env->GetArrayLength(jthresh) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Threshold array too short");
+        return;
+    }
+    if (env->GetArrayLength(jout) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Output array too short");
+        return;
+    }
+    if (width < 0 || height < 0) {
+         throwException(env, "java/lang/IllegalArgumentException", "Width and height must be non-negative");
+         return;
+     }
+
+    unsigned char *in = (unsigned char *) env->GetByteArrayElements(jin, 0);
+    unsigned char *thresh = (unsigned char *) env->GetByteArrayElements(jthresh, 0);
+    unsigned char *out = (unsigned char *) env->GetByteArrayElements(jout, 0);
+
+    for (int i = 0;  i < height;  i++) {
+        for (int j = 0;  j < width;  j++) {
+            unsigned char thr = getPixel(in, width, i, j);
+            unsigned char val = getPixel(thresh, width, i, j);
+            setPixel(out, width, i, j, (thr - val < offset) ? (unsigned char)hi : (unsigned char)lo);
+        }
+    }
+
+    env->ReleaseByteArrayElements(jin, (jbyte *)in, 0);
+    env->ReleaseByteArrayElements(jthresh, (jbyte *)thresh, 0);
+    env->ReleaseByteArrayElements(jout, (jbyte *)out, 0);
+}
+
+
---
+++ src/net/bitquill/ocr/PreviewImage.java
@@ -0,0 +1,168 @@
+package net.bitquill.ocr;
+
+import java.io.IOException;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.util.Log;
+
+public class PreviewImage {
+    
+    private static final String TAG = "PreviewImage";
+    
+    static {
+        try {
+            System.loadLibrary("ocr");
+        } catch (UnsatisfiedLinkError ule) {
+            Log.e(TAG, "Could not load native library ocr", ule);
+        }   
+    }
+    
+    private byte[] mData;
+    private int mWidth;
+    private int mHeight;
+    
+    // TODO - decide method scope
+    protected PreviewImage (int width, int height) {
+        mWidth = width;
+        mHeight = height;
+        mData = new byte[width * height];
+    }
+
+    public PreviewImage (byte[] data, int width, int height) {
+        if (data.length < width * height) {
+            throw new IllegalArgumentException("Image data array is too short");
+        }
+        mData = data;
+        mWidth = width;
+        mHeight = height;
+    }
+    
+    /**
+     * Copy constructor.
+     * @param other  PreviewImage to copy from.
+     */
+    public PreviewImage (PreviewImage other) {
+        int width = other.mWidth;
+        int height = other.mHeight;
+        mWidth = width;
+        mHeight = height;
+        mData = new byte[width*height];
+        System.arraycopy(other.mData, 0, mData, 0, width * height);
+    }
+    
+    public final int getWidth () {
+        return mWidth;
+    }
+    
+    public final int getHeight () {
+        return mHeight;
+    }
+    
+    public final int getPixel (int i, int j) {
+        return PreviewImage.getPixel(mData, mWidth, i, j);
+    }
+    
+    public final void setPixel (int i, int j, byte value) {
+        PreviewImage.setPixel(mData, mWidth, i, j, value);
+    }
+    
+    public final void getRow (int i, byte[] values) {
+        int width = mWidth;
+        System.arraycopy(mData, i*width, values, 0, width);
+    }
+    
+    // FIXME - remove this method??
+    public final byte[] getData () {
+        return mData;
+    }
+    
+    public int mean () {
+        return PreviewImage.nativeMean(mData, mWidth, mHeight);
+    }
+    
+    public PreviewImage meanFilter (int radius, PreviewImage dest) {
+        if (dest.mWidth != mWidth || dest.mHeight != mHeight) {
+            throw new IllegalArgumentException("Destination image size must match");
+        }
+        PreviewImage.nativeMeanFilter(mData, dest.mData, mWidth, mHeight, radius);
+        return dest;
+    }
+    
+    public PreviewImage meanFilter (int radius) {
+        return meanFilter(radius, new PreviewImage(mWidth, mHeight));
+    }
+    
+    public PreviewImage adaptiveThreshold (byte hi, byte lo, int offset, PreviewImage thresh, PreviewImage dest) {
+        int width = mWidth;
+        int height = mHeight;
+        if (thresh.mWidth != width || thresh.mHeight != height) {
+            throw new IllegalArgumentException("Threshold image size must match");
+        }
+        if (dest.mWidth != width || dest.mHeight != height) {
+            throw new IllegalArgumentException("Destination image size must match");            
+        }
+        PreviewImage.nativeAdaptiveThreshold(mData, thresh.mData, dest.mData, width, height, hi, lo, offset);
+        return dest;
+    }
+    
+    public Bitmap asBitmap (int left, int top, int width, int height, int[] buf) {
+        if (buf == null) {
+            throw new NullPointerException("Buffer is null");
+        }
+        int imgWidth = mWidth;
+        int imgHeight = mHeight;
+        nativeGrayToARGB(mData, imgWidth, imgHeight, buf, left, top, width, height);
+        Bitmap b = Bitmap.createBitmap(buf, width, height, Config.ARGB_8888);
+        return b;        
+    }
+    
+    public Bitmap asBitmap (int left, int top, int width, int height) {
+        return asBitmap(left, top, width, height, new int[width*height]);
+    }
+    
+    public Bitmap asBitmap (int[] buf) {
+        return asBitmap(0, 0, mWidth, mHeight, buf);
+    }
+    
+    public Bitmap asBitmap () {
+        return asBitmap(0, 0, mWidth, mHeight);
+    }
+     
+    private static final int getPixel (byte[] data, int width, int i, int j) {
+        return data[i*width + j] & 0xFF;
+    }
+
+    private static final void setPixel (byte[] data, int width, int i, int j, byte value) {
+        data[i*width + j] = value;
+    }
+        
+    /**
+     * Adaptive mean filter for grayscale image, using a square filter of given radius.
+     * 
+     * Does not perform any parameter checking, so you must ensure that:
+     * (i) radius does not exceed width or height;
+     * (ii) 256*(2*radius+1)^2 fits in a 32-bit integer;
+     * (iii) the output byte array is sufficiently large.
+     * 
+     * @param in  Input grayscale image
+     * @param out Output filtered grayscale image
+     * @param width  Width of input image
+     * @param height Height of input image
+     * @param radius Radius of mean filter; diameter is 2*radius + 1
+     * @return Overall image intensity mean
+     */    
+    native private static void nativeMeanFilter (byte[] in, byte[] out, int width, int height, int radius);
+    
+    native private static void nativeAdaptiveThreshold (byte[] in, byte[] thresh, byte[] out, int width, int heigth, byte hi, byte lo, int offset);
+    
+    native private static int nativeMean (byte[] in, int width, int height);
+    
+    native private static void nativeGrayToARGB (byte[] in, int imgWidth, int imgHeight, int[] out, int left, int top, int width, int height);
+    
+    static public void decode (byte[] yuv, int width, int height) {
+        byte[] mean = new byte[width * height];
+        nativeMeanFilter(yuv, mean, width, height, 10);
+    }
+    
+}
