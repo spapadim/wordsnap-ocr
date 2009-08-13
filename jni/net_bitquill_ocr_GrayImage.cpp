fd1f2027aba484874f0131f377684491411d46c2
spapadim <spapadim@b9292112-8e80-11de-9a8c-6d5b7d1e4d13>
2009-08-13 04:56:53 +0000

    Untested implementation of erosion and dilation structural operators.

--- jni/net_bitquill_ocr_GrayImage.cpp
+++ jni/net_bitquill_ocr_GrayImage.cpp
@@ -9,16 +9,20 @@ static void throwException (JNIEnv *env, const char* ex, const char* msg)
     }
 }
 
+inline static int linearIndex (int width, int i, int j) {
+    return i*width + j;
+}
+
 template <class T>
 inline static T getPixel (const T data[], int width, int i, int j)
 {
-    return data[i*width + j];
+    return data[linearIndex(width, i, j)];
 }
 
 template <class T>
 inline static void setPixel (T data[], int width, int i, int j, T value)
 {
-    data[i*width + j] = value;
+    data[linearIndex(width, i, j)] = value;
 }
 
 template <class T>
@@ -245,4 +249,151 @@ JNIEXPORT void JNICALL Java_net_bitquill_ocr_GrayImage_nativeAdaptiveThreshold
     env->ReleaseByteArrayElements(jout, (jbyte *)out, 0);
 }
 
+template<class Op>
+static inline unsigned char borderStructuralTransform
+    (const unsigned char* in, int width, int height,
+            int i0, int j0,
+            int numNeighbors,
+            const int *hOffsets, const int *vOffsets,
+            Op op, unsigned char val0)
+{
+    unsigned char val = val0;
+    for (int n = 0;  n < numNeighbors;  n++) {
+        int i = i0 + vOffsets[n];
+        int j = j0 + hOffsets[n];
+        if (i >= 0 && i < height && j >= 0 && j < width) {
+            val = op(val, getPixel(in, width, i, j));
+        }
+    }
+    return val;
+}
 
+template<class Op>
+static void structuralTransform
+  (JNIEnv *env, jclass cls,
+          jbyteArray jin, jbyteArray jout, jint width, jint height,
+          jint numNeighbors,
+          jintArray jhOffsets, jintArray jvOffsets,
+          jintArray jlinearOffsets,
+          jint minX, jint maxX, jint minY, jint maxY,
+          Op op, unsigned char val0)
+{
+    // Check parameters
+    if (env->GetArrayLength(jin) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Input array too short");
+        return;
+    }
+    if (env->GetArrayLength(jout) < width * height) {
+        throwException(env, "java/lang/IllegalArgumentException", "Output array too short");
+        return;
+    }
+    if (width < 0 || height < 0) {
+         throwException(env, "java/lang/IllegalArgumentException", "Width and height must be non-negative");
+         return;
+    }
+    // TODO - more:
+    // minX,minY <= 0  maxX, maxY >= 0
+    // numNeighbors >= 1
+    // offset array lengths == numNeighbors
+
+    unsigned char *in = (unsigned char *) env->GetByteArrayElements(jin, 0);
+    unsigned char *out = (unsigned char *) env->GetByteArrayElements(jout, 0);
+    int *hOffsets = (int *) env->GetIntArrayElements(jhOffsets, 0);
+    int *vOffsets = (int *) env->GetIntArrayElements(jvOffsets, 0);
+    int *linearOffsets = (int *) env->GetIntArrayElements(jlinearOffsets, 0);
+
+    // Top edge
+    for (int i0 = 0;  i0 < -minY;  i0++) {
+        for (int j0 = 0;  j0 < width;  j0++) {
+            setPixel(out, width, i0, j0,
+                    borderStructuralTransform(in, width, height,
+                            i0, j0, numNeighbors, hOffsets, vOffsets, op, val0));
+        }
+    }
+    // Bottom edge
+    for (int i0 = height - maxY;  i0 < height;  i0++) {
+        for (int j0 = 0;  j0 < width;  j0++) {
+            setPixel(out, width, i0, j0,
+                    borderStructuralTransform(in, width, height,
+                            i0, j0, numNeighbors, hOffsets, vOffsets, op, val0));
+        }
+    }
+    // Left edge
+    for (int i0 = -minY;  i0 < height - maxY;  i0++) {
+        for (int j0 = 0;  j0 < -minX;  j0++) {
+            setPixel(out, width, i0, j0,
+                    borderStructuralTransform(in, width, height,
+                            i0, j0, numNeighbors, hOffsets, vOffsets, op, val0));
+        }
+    }
+    // Right edge
+    for (int i0 = -minY;  i0 < height - maxY;  i0++) {
+        for (int j0 = width - maxX;  j0 < width;  j0++) {
+            setPixel(out, width, i0, j0,
+                    borderStructuralTransform(in, width, height,
+                            i0, j0, numNeighbors, hOffsets, vOffsets, op, val0));
+        }
+    }
+
+    // Interior pixels
+    for (int i0 = -minY;  i0 < height - maxY;  i0++) {
+        for (int j0 = -minX;  j0 < width - maxX;  j0++) {
+            int lin0 = linearIndex(width, i0, j0);
+            unsigned char val = in[lin0 + linearOffsets[0]];
+            for (int n = 1;  n < numNeighbors;  n++) {
+                val = op(val, in[lin0 + linearOffsets[n]]);
+            }
+            setPixel(out, width, i0, j0, val);
+        }
+    }
+
+    env->ReleaseByteArrayElements(jin, (jbyte *)in, 0);
+    env->ReleaseByteArrayElements(jout, (jbyte *)out, 0);
+    env->ReleaseIntArrayElements (jhOffsets, (jint *)hOffsets, 0);
+    env->ReleaseIntArrayElements (jvOffsets, (jint *)vOffsets, 0);
+    env->ReleaseIntArrayElements (jlinearOffsets, (jint *)linearOffsets, 0);
+}
+
+static struct MinOp
+{
+    inline unsigned char operator () (unsigned char a, unsigned char b) {
+        return (a < b) ? a : b;
+    }
+} minOp;
+
+JNIEXPORT void JNICALL Java_net_bitquill_ocr_GrayImage_nativeErode
+  (JNIEnv *env, jclass cls,
+          jbyteArray jin, jbyteArray jout, jint width, jint height,
+          jint numNeighbors,
+          jintArray jhOffsets, jintArray jvOffsets,
+          jintArray jlinearOffsets,
+          jint minX, jint maxX, jint minY, jint maxY)
+{
+    structuralTransform(env, cls,
+            jin, jout, width, height,
+            numNeighbors, jhOffsets, jvOffsets, jlinearOffsets,
+            minX, maxX, minY, maxY,
+            minOp, 255);
+}
+
+static struct maxOp
+{
+    inline unsigned char operator () (unsigned char a, unsigned char b) {
+        return (a > b) ? a : b;
+    }
+} maxOp;
+
+JNIEXPORT void JNICALL Java_net_bitquill_ocr_GrayImage_nativeDilate
+  (JNIEnv *env, jclass cls,
+          jbyteArray jin, jbyteArray jout, jint width, jint height,
+          jint numNeighbors,
+          jintArray jhOffsets, jintArray jvOffsets,
+          jintArray jlinearOffsets,
+          jint minX, jint maxX, jint minY, jint maxY)
+{
+    structuralTransform(env, cls,
+            jin, jout, width, height,
+            numNeighbors, jhOffsets, jvOffsets, jlinearOffsets,
+            minX, maxX, minY, maxY,
+            maxOp, 0);
+}
